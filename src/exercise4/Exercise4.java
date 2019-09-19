e854c145b8ef3541c23c2838c5c0bb4d36a45357
ferolinma_cis21035 <ferolinma_cis21035@ADMINRG-SMD0A2G.ceaccs.net>
2019-09-19 14:36:29 +0800

    Setter to set values if no arguments passed during instantiation.

--- src/exercise4/Exercise4.java
+++ src/exercise4/Exercise4.java
@@ -12,13 +12,16 @@ package exercise4;
 public class Exercise4 {
 
     public static void main(String[] args) {
-        Rectangle shape = new Rectangle (1.0f, 1.0f);
+        Rectangle shape = new Rectangle ();
+        shape.setLength(1.0f);
+        shape.setWidth(1.0f);
         System.out.println("RECTANGLE\n");
         System.out.println(shape.toString());
         System.out.println("Area: " + shape.getArea());
         System.out.println("Perimeter: " + shape.getPerimeter());
         System.out.print("\n");
-        Circle shape1 = new Circle(1.0f);
+        Circle shape1 = new Circle();
+        shape1.setRadius(1.0);
         System.out.println("CIRCLE\n");
         System.out.println(shape1.toString());
         System.out.println("Area: " + shape1.getArea());
