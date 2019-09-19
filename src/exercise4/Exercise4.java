d3e58647cc1fa649f3ad42d81eb9b5611dbe143b
ferolinma_cis21035 <ferolinma_cis21035@ADMINRG-SMD0A2G.ceaccs.net>
2019-09-19 13:53:45 +0800

    Additional print statements in main()

--- src/exercise4/Exercise4.java
+++ src/exercise4/Exercise4.java
@@ -14,10 +14,17 @@ public class Exercise4 {
     public static void main(String[] args) {
         Rectangle shape = new Rectangle ();
         shape.Rectangle(1.0f, 1.0f);
+        System.out.println("RECTANGLE\n");
         System.out.println(shape.toString());
+        System.out.println("Area: " + shape.getArea());
+        System.out.println("Perimeter: " + shape.getPerimeter());
+        System.out.print("\n");
         Circle shape1 = new Circle();
         shape1.Circle(1.0);
+        System.out.println("CIRCLE\n");
         System.out.println(shape1.toString());
+        System.out.println("Area: " + shape1.getArea());
+        System.out.println("Circumference: " + shape1.getCircumference());
     }
     
 }
